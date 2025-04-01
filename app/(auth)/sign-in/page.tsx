46b80440bb1d6632d4aa3469fed4cd36fc22ce8b
Telmen <telmenganhuyag@gmail.com>
2025-04-02 03:36:59 +0800

    ?

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -18,14 +18,14 @@ export const metadata: Metadata = {
 };
 
 const SignInPage = async (props: {
-  // searchParams: Promise<{ callbackUrl: string }>;
+  searchParams: Promise<{ callbackUrl: string }>;
 }) => {
-  // const { callbackUrl } = await props.searchParams;
-  // const session = await auth();
+  const { callbackUrl } = await props.searchParams;
+  const session = await auth();
 
-  // if (session) {
-  //   return redirect(callbackUrl || '/');
-  // }
+  if (session) {
+    return redirect(callbackUrl || '/');
+  }
 
   return (
     <div className="w-full max-w-md mx-auto">
