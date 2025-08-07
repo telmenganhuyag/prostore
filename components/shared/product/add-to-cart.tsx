0b6f6756539f8f718e207f2e814f2237b410fd19
Telmen <telmenganhuyag@gmail.com>
2025-08-07 22:58:54 +0800

    remove cart action ~ dynamic cart button

--- components/shared/product/add-to-cart.tsx
+++ components/shared/product/add-to-cart.tsx
@@ -2,12 +2,12 @@
 
 import { Button } from '@/components/ui/button';
 import { useRouter } from 'next/navigation';
-import { Plus } from 'lucide-react';
-import { CartItem } from '@/types';
+import { Plus, Minus } from 'lucide-react';
+import { Cart, CartItem } from '@/types';
 import { toast } from 'sonner';
-import { addItemToCart } from '@/lib/actions/cart.actions';
+import { addItemToCart, removeItemFromCart } from '@/lib/actions/cart.actions';
 
-const AddToCart = ({ item }: { item: CartItem }) => {
+const AddToCart = ({ cart, item }: { cart?: Cart; item: CartItem }) => {
   const router = useRouter();
   const handleAddToCart = async () => {
     const res = await addItemToCart(item);
@@ -32,7 +32,25 @@ const AddToCart = ({ item }: { item: CartItem }) => {
     });
   };
 
-  return (
+  const handleRemoveFromCart = () => {
+    return;
+  };
+
+  // Check if item is in cart
+  const existItem =
+    cart && cart.items.find((x) => x.productId === item.productId);
+
+  return existItem ? (
+    <div>
+      <Button type="button" variant="outline" onClick={handleRemoveFromCart}>
+        <Minus className="h-4 w-4" />
+      </Button>
+      <span className="px-2">{existItem.qty}</span>
+      <Button type="button" variant="outline" onClick={handleAddToCart}>
+        <Plus className="h-4 w-4" />
+      </Button>
+    </div>
+  ) : (
     <Button
       className="w-full cursor-pointer"
       type="button"
