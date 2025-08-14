696688f744b4645121533b9b93f7039257f8fbc1
Telmen <telmenganhuyag@gmail.com>
2025-08-15 01:11:35 +0800

    next

--- components/shared/product/add-to-cart.tsx
+++ components/shared/product/add-to-cart.tsx
@@ -1,39 +1,50 @@
 'use client';
-
 import { Button } from '@/components/ui/button';
 import { useRouter } from 'next/navigation';
-import { Plus, Minus } from 'lucide-react';
+import { Plus, Minus, Loader } from 'lucide-react';
 import { Cart, CartItem } from '@/types';
 import { toast } from 'sonner';
 import { addItemToCart, removeItemFromCart } from '@/lib/actions/cart.actions';
+import { useTransition } from 'react';
 
 const AddToCart = ({ cart, item }: { cart?: Cart; item: CartItem }) => {
   const router = useRouter();
+
+  const [isPending, startTransition] = useTransition();
+
   const handleAddToCart = async () => {
-    const res = await addItemToCart(item);
+    startTransition(async () => {
+      const res = await addItemToCart(item);
 
-    if (!res.success) {
-      toast.error('This is an error message');
-      return;
-    }
+      if (!res.success) {
+        toast.error(res.message);
+        return;
+      }
 
-    // Handle success add to cart
-    toast(res.message, {
-      action: (
-        <Button
-          className="bg-primary cursor-pointer text-white hover:bg-gray-800"
-          onClick={() => {
-            router.push('/cart');
-          }}
-        >
-          Go To Cart
-        </Button>
-      ),
+      // Handle success add to cart
+      toast(res.message, {
+        action: (
+          <Button
+            className="bg-primary cursor-pointer text-white hover:bg-gray-800"
+            onClick={() => {
+              router.push('/cart');
+            }}
+          >
+            Go To Cart
+          </Button>
+        ),
+      });
     });
   };
 
-  const handleRemoveFromCart = () => {
-    return;
+  const handleRemoveFromCart = async () => {
+    startTransition(async () => {
+      const res = await removeItemFromCart(item.productId);
+
+      res?.success
+        ? toast(res.message)
+        : toast.error(res?.message || 'Something went wrong');
+    });
   };
 
   // Check if item is in cart
@@ -43,11 +54,19 @@ const AddToCart = ({ cart, item }: { cart?: Cart; item: CartItem }) => {
   return existItem ? (
     <div>
       <Button type="button" variant="outline" onClick={handleRemoveFromCart}>
-        <Minus className="h-4 w-4" />
+        {isPending ? (
+          <Loader className="w-4 h-4 animate-spin" />
+        ) : (
+          <Minus className="h-4 w-4" />
+        )}
       </Button>
       <span className="px-2">{existItem.qty}</span>
       <Button type="button" variant="outline" onClick={handleAddToCart}>
-        <Plus className="h-4 w-4" />
+        {isPending ? (
+          <Loader className="w-4 h-4 animate-spin" />
+        ) : (
+          <Plus className="h-4 w-4" />
+        )}
       </Button>
     </div>
   ) : (
@@ -56,7 +75,11 @@ const AddToCart = ({ cart, item }: { cart?: Cart; item: CartItem }) => {
       type="button"
       onClick={handleAddToCart}
     >
-      <Plus />
+      {isPending ? (
+        <Loader className="w-4 h-4 animate-spin" />
+      ) : (
+        <Plus className="h-4 w-4" />
+      )}
       Add To Cart
     </Button>
   );
--- lib/actions/cart.actions.ts
+++ lib/actions/cart.actions.ts
@@ -189,6 +189,10 @@ export async function removeItemFromCart(productId: string) {
     });
 
     revalidatePath(`/product/${product.slug}`);
+    return {
+      success: true,
+      message: `${product.name} was removed from cart`,
+    };
   } catch (error) {
     return { success: false, message: formatError(error) };
   }
